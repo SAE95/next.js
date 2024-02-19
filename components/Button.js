898c1f46a284f30a260deb4afad44c9fae5f7971
SAE95 <73691231+SAE95@users.noreply.github.com>
2024-02-19 18:27:48 +0100

    tested component

---
+++ components/Button.js
@@ -0,0 +1,9 @@
+import PrimaryButton from "https://framer.com/m/Primary-button-uQcy.js@0jJkQXUVBw9Ce0z7gGXL"
+
+
+export function Button() {
+    return ( 
+    <PrimaryButton/>
+
+    )
+}
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import Head from "next/head";
 import { Tip } from "../components/Tip";
 import { Container } from "../components/Container";
+import { Button } from "../components/Button";
+
+
 
 // A Smart Component from Framer
 import Toggle from "https://framer.com/m/Toggle-B5iT.js@52zFaz7rN7Bt3pjtYxWH";
+
 
 export default function Home() {
   return (
@@ -18,6 +22,8 @@ export default function Home() {
         <Toggle />
         <Tip />
       </Container>
+
+      
     </>
   );
 }
